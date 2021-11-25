f19f0c45d285c26667d740072199ec98aefa594a
Sina Nazari <sinanazari3000@gmail.com>
2021-11-25 18:15:18 +0330

    Add project files.

---
+++ Linq_Example_1/Linq_Example_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var location = Path.Combine(Directory.GetCurrentDirectory(),"CsvFile");
+
+        SortFilesWithOutLinq(location);
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("********************************************");
+        Console.ResetColor();
+
+        SorFilesWithLinq(location);
+
+    }
+
+    public static void SorFilesWithLinq(string location)
+    {
+        var files = new DirectoryInfo(location).GetFiles()
+            .OrderBy(f => f.Length).Take(5);
+        
+        foreach (var file in files)
+        {
+            Console.WriteLine($"Name:{file.Name} ************ Length:{file.Length}");
+        }
+    }
+
+    public static void SortFilesWithOutLinq(string location)
+    {
+        var DirInfo = new DirectoryInfo(location);
+        FileInfo[] files = DirInfo.GetFiles();
+
+        Array.Sort(files, (x, y) =>
+        {
+            if (x.Length == y.Length)
+            {
+                return 0;
+            }else if (x.Length > y.Length)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        });
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            FileInfo file = files[i];
+            Console.WriteLine($"Name:{file.Name} ************ Length:{file.Length}");
+        }
+    }
+}
---
+++ Linq_Example_2/Linq_Example_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace Linq_Example_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var location = Path.Combine(Directory.GetCurrentDirectory(), "CvsFile");
+
+            SortfilesWhitOutLinq(location);
+
+            
+        }
+
+        public static void SortfilesWhitOutLinq(string location)
+        {
+            var DirInfo = new DirectoryInfo(location);
+            FileInfo[] files = DirInfo.GetFiles();
+
+            Array.Sort(files, (x, y) =>
+            {
+                if (x.Length == y.Length)
+                {
+                    return 0;
+                }
+                else if (x.Length > y.Length)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+
+            });
+
+            for (int i = 0; i <files.Length; i++)
+            {
+                FileInfo file = files[i];
+
+                Console.WriteLine($"{file.Name}****** {file.Length}");
+            }
+        }
+    }
+}
