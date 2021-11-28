49f7198741a2ff18ff1fd815236a6e594b43619a
Sina Nazari <sinanazari3000@gmail.com>
2021-11-28 05:31:26 +0330

    Update Or Create Source Code

---
+++ Linq_Example_4/Linq_Example_4/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>()
+        {
+            0, 17, 18,19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        };
+
+        var names = new List<string>()
+        {
+            "Sina","Ali","Reza","Mina","Sara","Darya"
+        };
+
+
+        var result = numbers.Where(n => n % 2 == 0).ToList();
+
+        var result2 = names.Where(n => n.Contains("e")).ToList();
+
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"Number is: {item}");
+        }
+
+        Console.WriteLine("*******************************");
+
+
+        foreach (var item in result2)
+        {
+            Console.WriteLine($"Name is: {item}");
+        }
+    }
+}
